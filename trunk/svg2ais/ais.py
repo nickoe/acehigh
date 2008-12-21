c3c1496a3ce4b77aa5b27af5d04ee85899813599
kkjaergaard <kkjaergaard@e1515860-bd22-11dd-b57b-7192f494c34b>
2008-12-21 22:03:59 +0000

    Tilføjede instruktioner til ais.py

--- trunk/svg2ais/ais.py
+++ trunk/svg2ais/ais.py
@@ -9,5 +9,38 @@
 #                                                           Kristian Kjærgaard
 # ----------------------------------------------------------------------------
 
+
+# Denne fil beskriver AceHigh Instruction Stream - dataformatet til vores plotter
+
+
+# Todo: jeg mangler en smart måde at bruge værdierne på: jeg skal kunne tilgå både byten og navnet (begge skal bruges uden for filen)
+# værdierne skal også omsættes til bytes så de skrives rigtigt til outputtet
+
+# tjek at det virker som det skal
+def Empty():
+  return 0
+
+ins = {
+  Empty:      0,
+  'Start':      1,
+  'Stop':       2,
+  'Up':         3,
+  'UpRight':    4,
+  'Right':      5,
+  'DownRight':  6,
+  'Down':       7,
+  'DownLeft':   8,
+  'Left':       9,
+  'UpLeft':     10,
+  'LowerHead':  11,
+  'LiftHead':   12
+}
+
+
 def getIns():
-  return "Jipperdipper"
+  return "Instruktion her"
+
+def getByte():
+  return "Byte her"
+
+print Empty()
