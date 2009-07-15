816e1e6ec92582f9669dcddda60ef2a1f1be4c4b
kkjaergaard <kkjaergaard@e1515860-bd22-11dd-b57b-7192f494c34b>
2009-07-15 18:36:32 +0000

    Added article about communication protocol

--- trunk/firmware/main.c
+++ trunk/firmware/main.c
@@ -4,15 +4,34 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 #include "steppers.h"
 #include "serial.h"
 
 #define PIN_LED 5
 
 
+int counter = 0;
+
+
+ISR(TIMER2_COMPA_vect)
+{
+  if (++counter == 1000) {
+    PORTB ^= 1<<PIN_LED;
+    counter = 0;
+  }
+}
+
 int main()
 {
   DDRB |= 1<<PIN_LED;
+
+  /* set timer/counter2 to interrupt */
+  TCCR2A = 1<<WGM21; /* ctc mode */
+  TCCR2B = 1<<CS21; /* prescaler to 8 */
+  OCR2A = 249; /* interrupts with 10 KHz */
+  TIMSK2 |= 1<<OCIE2A; /* enable interrupt */
+  
 
   sei();
   serial_init();
