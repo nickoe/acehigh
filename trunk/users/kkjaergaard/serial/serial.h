84efc363cb56e9527e0004f6f2b4b4da68cf0f0f
kkjaergaard <kkjaergaard@e1515860-bd22-11dd-b57b-7192f494c34b>
2009-07-24 11:15:20 +0000

    Working output buffer for serial communication

--- trunk/users/kkjaergaard/serial/serial.h
+++ trunk/users/kkjaergaard/serial/serial.h
@@ -1,4 +1,6 @@
 /*
+ * Communication library for AceHigh
+ *
  * $Id$
  */
 
@@ -6,12 +8,28 @@
 #define __SERIAL_H__
 
 
-/* Initializes serial communication and command buffering, and starts
+/*
+ * Initializes serial communication and command buffering, and starts
  * acting when data i recieved. Return zero on succes.
+ *
+ * The serial interface is communicating using RS-232, at 9600 baud,
+ * 8N1.
  */
 int serial_init(void);
 
+/*
+ * Sends a single char throuth the serial interface. The char will be
+ * sent as soon as the interface is ready. Returns when the byte has
+ * been buffered.
+ */
 void serial_putc(char c);
+
+/*
+ * Sends a character string through the serial interface. The string
+ * will be sent as soon as the interface is ready. Returns when the
+ * string has been buffered.
+ */
+void serial_puts(const char *s);
 
 
 #endif /* __SERIAL_H__ */
