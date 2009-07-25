a8f424cdd1bb1f912399430a65f57774a2093999
kkjaergaard <kkjaergaard@e1515860-bd22-11dd-b57b-7192f494c34b>
2009-07-25 11:45:28 +0000

    Added doxygen makefile for easy API documentation of entire software

--- trunk/users/kkjaergaard/serial/serial.c
+++ trunk/users/kkjaergaard/serial/serial.c
@@ -11,41 +11,60 @@
 
 
 /* io buffers */
-#define RX_BUF_SIZE 256
-char volatile rx_buf[RX_BUF_SIZE];
-uint8_t volatile rx_head = 0;
-uint8_t volatile rx_count = 0;
+#define RX_BUF_SIZE 64 ///< Size of input buffer
+char volatile rx_buf[RX_BUF_SIZE]; ///< Input buffer
+uint8_t volatile rx_head = 0; ///< Input buffer head
+uint8_t volatile rx_count = 0; ///< Number of byte in input buffer
 
-#define TX_BUF_SIZE RX_BUF_SIZE
-char volatile tx_buf[TX_BUF_SIZE];
-uint8_t volatile tx_head = 0;
-uint8_t volatile tx_count = 0;
+#define TX_BUF_SIZE RX_BUF_SIZE ///< Size of output buffer
+char volatile tx_buf[TX_BUF_SIZE]; ///< Output buffer
+uint8_t volatile tx_head = 0; ///< Output buffer head
+uint8_t volatile tx_count = 0; ///< Number of bytes in output buffer
 
 
 /* initialize */
-int serial_init(void)
+void serial_init(void)
 {
-  /* interrupt on recieve complete, enable reciever and transmitter */
-  UCSR0B = (1<<RXCIE0) | (1<<RXEN0) | (1<<TXEN0);
   /* 8N1 - 8 data bits, no parity, one stopbit */
   UCSR0C = (1<<UCSZ01) | (1<<UCSZ00);
   /* 9600 baud (cpu frequency is 20 MHz */
   UBRR0L = 129;
-
-  return 0;
 }
 
 
-/* interrupt vector executed when data i recieved */
+/* clear input and output buffer, enable reciever, start acting upon
+   signals */
+void serial_enable(void)
+{
+  rx_count = 0;
+  tx_count = 0;
+  /* interrupt on recieve complete, enable reciever and transmitter */
+  UCSR0B |= (1<<RXCIE0) | (1<<RXEN0) | (1<<TXEN0);
+}
+
+
+/* disables reciever and transmitter */
+void serial_disable(void)
+{
+  /* disable reciever, transmitter and data register empty
+     interrupt */
+  UCSR0B &= ~((1<<RXEN0) | (1<<TXEN0) | (1<<UDRIE0));
+}
+
+
+/// Interrupt executed when data is recieved
 ISR(USART_RX_vect)
 {
   if (rx_count == RX_BUF_SIZE) {
-    /* input buffer is full and we have a very big problem */
+    /* input buffer is full and we have a very big problem, respond
+       with "packet too big" */
     return;
   }
 
   rx_buf[(rx_head + rx_count) % RX_BUF_SIZE] = UDR0;
   rx_count++;
+
+  /* act upon the signal here */
 }
 
 
@@ -66,14 +85,24 @@ void serial_putc(char c)
 }
 
 
-/* sends data through the usart, returns when data has been buffered */
+/* sends data through the usart, returns when data has been buffered,
+   string must be null-terminated */
 void serial_puts(const char *s)
 {
   while (*s)
     serial_putc(*s++);
 }
 
-/* data register i ready to recieve data */
+
+/* sends binary data stream through the usart */
+void serial_putb(char *s, int len)
+{
+  for (int i = 0; i < len; i++)
+    serial_putc(*s++);
+}
+
+
+/// Interrupt executed when data register is ready to reviece data
 ISR(USART_UDRE_vect)
 {
   /* dequeue transmit buffer*/
